33d40658e2b1ea07e1df981a509fc5748d38632e
Yuhei Jotaki <jotaki@cloudpack.jp>
2019-08-20 09:16:33 +0900

    update qa.json

--- src/components/Lead.vue
+++ src/components/Lead.vue
@@ -13,8 +13,8 @@ export default {
   name: 'lead',
   data () {
     return {
-      leadTitle: '📙 AWS 認定クラウドプラクティショナー 模擬試験サンプル問題',
-      leadText: '模擬試験のサンプル問題です。（2019年5月受験）<br>正答が公開されないため解答は確実ではありませんが、90%以上取れたので <span class="u-bold">だいたい</span> あってます。<br><span class="u-bold u-emphasis">試験問題オープン（Web上/SNSで共有など）にするとAWSに怒られるそうなので、クローズド（SNSで共有しない/個人観賞用）でお願いします。</span>'
+      leadTitle: 'vue-quiz',
+      leadText: '<a href="https://d1.awsstatic.com/training-and-certification/Docs%20-%20Cloud%20Practitioner/AWS%20Certified%20Cloud%20Practioner_Sample%20Questions_v1.1_FINAL.PDF" target="_blank">AWS Certified Cloud Practitioner – Foundational (CLF-C01) Sample Exam Questions</a> からの抜粋です。'
     }
   }
 }
